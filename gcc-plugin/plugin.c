f177183bf2891f02473c1fb382e0e1be896074d7
Kamil Dudka <kdudka@redhat.com>
2009-07-23 19:47:49 +0200

    gcc-plugin: now can be loaded by gcc

--- gcc-plugin/plugin.c
+++ gcc-plugin/plugin.c
@@ -1,1 +1,10 @@
 #include <gcc-plugin.h>
+
+int plugin_is_GPL_compatible;
+
+int plugin_init (struct plugin_name_args *plugin_info,
+                 struct plugin_gcc_version *version)
+{
+    fprintf(stderr, "--- initialized\n");
+    return 0;
+}
