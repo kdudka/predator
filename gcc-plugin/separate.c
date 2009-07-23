23fce6c787391c0964c9708bf1ab6a5ab02c2f59
Kamil Dudka <kdudka@redhat.com>
2009-07-23 21:17:21 +0200

    rename plugin -> separate

--- gcc-plugin/separate.c
+++ gcc-plugin/separate.c
@@ -1,20 +1,20 @@
 #include <gcc-plugin.h>
 
-#define PLUGIN_LOG(...) do { \
+#define SEP_LOG(...) do { \
     fprintf (stderr, "--- %s: ", plugin_name); \
     fprintf (stderr, __VA_ARGS__); \
     fprintf (stderr, "\n"); \
 } while (0)
 
-#define PLUGIN_LOG_FNC \
-    PLUGIN_LOG ("%s", __FUNCTION__)
+#define SEP_LOG_FNC \
+    SEP_LOG ("%s", __FUNCTION__)
 
 int plugin_is_GPL_compatible;
 
 static const char *plugin_name = "[uninitialized]";
 
 static struct plugin_info info = {
-    .version = "plug-in 0.1 experimental",
+    .version = "gcc plug-in separate 0.1 experimental",
     .help = "print output program direct to stdout; verbose output to stderr",
 };
 
@@ -23,12 +23,12 @@ int plugin_init (struct plugin_name_args *plugin_info,
 {
     plugin_name = plugin_info->full_name;
 
-    PLUGIN_LOG_FNC;
-    PLUGIN_LOG ("using gcc %s %s, built at %s", version->basever,
+    SEP_LOG_FNC;
+    SEP_LOG ("using gcc %s %s, built at %s", version->basever,
                 version->devphase, version->datestamp);
 
     register_callback(plugin_info->base_name, PLUGIN_INFO, NULL, &info);
 
-    PLUGIN_LOG ("%s successfully initialized", plugin_info->version);
+    SEP_LOG ("'%s' successfully initialized", plugin_info->version);
     return 0;
 }
