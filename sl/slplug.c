bc2a701f0e1a2d40414fb25ee7094499c42dfc34
Kamil Dudka <kdudka@redhat.com>
2009-08-16 18:49:28 +0200

    slplug: some callback to code_listener

--- sl/slplug.c
+++ sl/slplug.c
@@ -1,6 +1,8 @@
 // this include has to be the first (according the gcc plug-in API)
 #include <gcc-plugin.h>
 
+#include "code_listener.h"
+
 #include <coretypes.h>
 #include <diagnostic.h>
 
@@ -16,19 +18,19 @@
 #include <signal.h>
 
 // TODO: replace with gcc native debugging infrastructure
-#define SEP_LOG(...) do { \
+#define SL_LOG(...) do { \
     fprintf (stderr, "--- %s: ", plugin_name); \
     fprintf (stderr, __VA_ARGS__); \
     fprintf (stderr, "\n"); \
 } while (0)
 
 // TODO: replace with gcc native debugging infrastructure
-#define SEP_LOG_FNC \
-    SEP_LOG ("%s", __FUNCTION__)
+#define SL_LOG_FNC \
+    SL_LOG ("%s", __FUNCTION__)
 
 // TODO: replace with gcc native debugging infrastructure
-#define SEP_WARN_UNHANDLED(what) \
-    SEP_LOG ("%s () at %s:%d: warning: '%s' not handled", \
+#define SL_WARN_UNHANDLED(what) \
+    SL_LOG ("%s () at %s:%d: warning: '%s' not handled", \
              __FUNCTION__, __FILE__, __LINE__, (what))
 
 // required by gcc plug-in API
@@ -38,26 +40,34 @@ int plugin_is_GPL_compatible;
 static const char *plugin_name = "[uninitialized]";
 
 // plug-in meta-data according to gcc plug-in API
-static struct plugin_info sep_info = {
-    .version = "gcc plug-in separate 0.1 experimental",
-    .help = "print output program directly to stdout; verbose output to stderr",
+static struct plugin_info sl_info = {
+    .version = "gcc plug-in slplug 0.1 [experimental]",
+    .help = "[not implemented]",
 };
+
+static struct cl_code_listener *cl = NULL;
 
 // print name of function argument ARG to stdout
 static void handle_fnc_decl_arg (tree arg)
 {
     tree ident = DECL_NAME (arg);
-    printf ("%s", IDENTIFIER_POINTER (ident));
+    (void) ident;//printf ("%s", IDENTIFIER_POINTER (ident));
 }
 
 // go through argument list ARGS of fnc declaration
 static void handle_fnc_decl_arglist (tree args)
 {
+    int argc = 0;
+
     while (args) {
         handle_fnc_decl_arg (args);
+
+        cl->fnc_arg_decl(cl, ++argc, IDENTIFIER_POINTER(
+                    DECL_NAME(args)));
+
         args = TREE_CHAIN (args);
-        if (args)
-            printf (", ");
+        /*if (args)
+            printf (", ");*/
     }
 }
 
@@ -99,24 +109,29 @@ static void handle_fnc_decl (tree decl)
 {
     // print function name
     tree ident = DECL_NAME (decl);
-    printf ("%s (", IDENTIFIER_POINTER (ident));
+    //printf ("%s (", IDENTIFIER_POINTER (ident));
+
+    cl->fnc_open(cl,
+            /* TODO */ 0,
+            /* TODO */ IDENTIFIER_POINTER(ident),
+            /* TODO */ CL_SCOPE_GLOBAL);
 
     // print argument list
     tree args = DECL_ARGUMENTS (decl);
     handle_fnc_decl_arglist (args);
-    printf (")\n");
+    //printf (")\n");
 
     // obtain fnc structure
     struct function *fnc = DECL_STRUCT_FUNCTION (decl);
     if (NULL == fnc) {
-        SEP_WARN_UNHANDLED ("NULL == fnc");
+        SL_WARN_UNHANDLED ("NULL == fnc");
         return;
     }
 
     // obtain gimple for fnc
     gimple_seq body = fnc->gimple_body;
     if (NULL == body) {
-        SEP_WARN_UNHANDLED ("gimple not found");
+        SL_WARN_UNHANDLED ("gimple not found");
         return;
     }
 
@@ -124,18 +139,20 @@ static void handle_fnc_decl (tree decl)
     printf ("{\n");
     handle_fnc_gimple (body);
     printf ("}\n\n");
+
+    cl->fnc_close(cl);
 }
 
 // callback of tree pass declared in <tree-pass.h>
-static unsigned int sep_pass_execute (void)
+static unsigned int sl_pass_execute (void)
 {
     if (!current_function_decl) {
-        SEP_WARN_UNHANDLED ("NULL == current_function_decl");
+        SL_WARN_UNHANDLED ("NULL == current_function_decl");
         return 0;
     }
 
     if (FUNCTION_DECL != TREE_CODE (current_function_decl)) {
-        SEP_WARN_UNHANDLED ("TREE_CODE (current_function_decl)");
+        SL_WARN_UNHANDLED ("TREE_CODE (current_function_decl)");
         return 0;
     }
 
@@ -144,24 +161,24 @@ static unsigned int sep_pass_execute (void)
 }
 
 // pass description according to <tree-pass.h> API
-static struct opt_pass sep_pass = {
-    .type = GIMPLE_PASS,
-    .name = "separate",
-    .gate = NULL,
-    .execute = sep_pass_execute,
-    .properties_required = PROP_gimple_any,
+static struct opt_pass sl_pass = {
+    .type                       = GIMPLE_PASS,
+    .name                       = "slplug",
+    .gate                       = NULL,
+    .execute                    = sl_pass_execute,
+    .properties_required        = PROP_gimple_any,
     // ...
 };
 
 // definition of a new pass provided by the plug-in
-static struct plugin_pass sep_plugin_pass = {
-    .pass                     = &sep_pass,
+static struct plugin_pass sl_plugin_pass = {
+    .pass                       = &sl_pass,
 
     // cfg ... control_flow_graph
-    .reference_pass_name      = "cfg",
+    .reference_pass_name        = "cfg",
 
-    .ref_pass_instance_number = 0,
-    .pos_op                   = PASS_POS_INSERT_BEFORE,
+    .ref_pass_instance_number   = 0,
+    .pos_op                     = PASS_POS_INSERT_BEFORE,
 };
 
 // callback called as last (if the plug-in does not crash before)
@@ -170,7 +187,10 @@ static void cb_finish (void *gcc_data, void *user_data)
     (void) gcc_data;
     (void) user_data;
 
-    SEP_LOG_FNC;
+    SL_LOG_FNC;
+
+    cl->destroy(cl);
+    cl_global_cleanup();
 }
 
 // callback called on start of input file processing
@@ -179,29 +199,43 @@ static void cb_start_unit (void *gcc_data, void *user_data)
     (void) gcc_data;
     (void) user_data;
 
-    SEP_LOG ("processing input file '%s'", main_input_filename);
+    SL_LOG ("processing input file '%s'", main_input_filename);
+    cl->file_open(cl, main_input_filename);
+}
+
+static void cb_finish_unit (void *gcc_data, void *user_data)
+{
+    (void) gcc_data;
+    (void) user_data;
+
+    SL_LOG_FNC;
+    cl->file_close(cl);
 }
 
 // register callbacks for plug-in NAME
-static void sep_regcb (const char *name) {
+static void sl_regcb (const char *name) {
     // passing NULL as CALLBACK to register_callback stands for virtual callback
 
     // register new pass provided by the plug-in
     register_callback (name, PLUGIN_PASS_MANAGER_SETUP,
                        /* callback */ NULL,
-                       &sep_plugin_pass);
+                       &sl_plugin_pass);
+
+    register_callback (name, PLUGIN_FINISH_UNIT,
+                       cb_finish_unit,
+                       /* data */ NULL);
 
     register_callback (name, PLUGIN_FINISH,
                        cb_finish,
-                       /* user_data */ NULL);
+                       /* data */ NULL);
 
     register_callback (name, PLUGIN_INFO,
                        /* callback */ NULL,
-                       &sep_info);
+                       &sl_info);
 
     register_callback (name, PLUGIN_START_UNIT,
                        cb_start_unit,
-                       /* user_data */ NULL);
+                       /* data */ NULL);
 }
 
 // plug-in initialization according to gcc plug-in API
@@ -218,12 +252,17 @@ int plugin_init (struct plugin_name_args *plugin_info,
 #endif
 
     // print something like "hello world!"
-    SEP_LOG_FNC;
-    SEP_LOG ("using gcc %s %s, built at %s", version->basever,
+    SL_LOG_FNC;
+    SL_LOG ("using gcc %s %s, built at %s", version->basever,
              version->devphase, version->datestamp);
 
+    cl = cl_code_listener_create("pp", STDOUT_FILENO, false);
+    gcc_assert(cl);
+
     // try to register callbacks (and virtual callbacks)
-    sep_regcb (plugin_info->base_name);
-    SEP_LOG ("'%s' successfully initialized", plugin_info->version);
+    sl_regcb (plugin_info->base_name);
+    SL_LOG ("'%s' successfully initialized", plugin_info->version);
+
+    cl_global_init_defaults(NULL, true);
     return 0;
 }
