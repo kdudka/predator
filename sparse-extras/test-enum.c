e8646bed6dd11df7237bb3c39ef2a13c4ae0752d
Kamil Dudka <kdudka@redhat.com>
2009-09-02 15:19:31 +0200

    sparse: respin of the last patch

--- sparse-extras/test-enum.c
+++ sparse-extras/test-enum.c
@@ -9,25 +9,30 @@ static void foo(void) {
     var_a = (enum ENUM_TYPE_A) VALUE_B;
     var_b = (enum ENUM_TYPE_B) i;
     i = (int) VALUE_A;
-    i = VALUE_B;
     anon_enum_var = VALUE_C;
     i = VALUE_C;
     i = anon_enum_var;
+    i = 7;
 
-    // already caught by -Wenum-mismatch (default)
+    // caught by -Wenum-mismatch (default) even without the patch
     var_a = var_b;
     var_b = anon_enum_var;
     anon_enum_var = var_a;
 
-    // caught by -Wint-to-enum (default)
+    // caught by -Wenum-mismatch (default) only with the patch applied
     var_a = VALUE_B;
     var_b = VALUE_C;
     anon_enum_var = VALUE_A;
+
+    // caught by -Wint-to-enum (default)
     var_a = 0;
     var_b = i;
     anon_enum_var = 0;
     anon_enum_var = i;
+    var_a = (int) VALUE_A;
+    var_a = (int) VALUE_B;
 
     // caught only with -Wenum-to-int
     i = var_a;
+    i = VALUE_B;
 }
