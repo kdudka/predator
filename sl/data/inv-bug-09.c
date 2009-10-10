0d97afd27bc486499d583c3fab6ff44d12a6baf9
Kamil Dudka <kdudka@redhat.com>
2009-10-10 14:17:10 +0200

    data/inv-bug-09.c: attempt to create dll of size 3

---
+++ sl/data/inv-bug-09.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+
+#define TRIGGER_INV_BUG 1
+
+typedef struct dll_item dll_item_t;
+typedef dll_item_t *dll_t;
+
+struct dll_item {
+    dll_item_t      *next;
+    dll_item_t      *prev;
+};
+
+#define DLL_NULL ((struct dll_item *) 0)
+#define DLL_NEW(type) \
+    ((type *) malloc(sizeof(type)))
+
+static dll_item_t* create_item(void)
+{
+    dll_item_t *item = DLL_NEW(dll_item_t);
+    if (!item)
+        abort();
+
+    item->next = DLL_NULL;
+    item->prev = DLL_NULL;
+
+    return item;
+}
+
+int main()
+{
+    /* attempt to create dll of size 3 */
+    dll_t list = create_item();
+    list->next = create_item();
+    list->next->prev = list;
+#if TRIGGER_INV_BUG
+    list->next->next = create_item();
+    list->next->next->prev = list->next;
+#endif
+
+    /* destroy dll */
+    while (list) {
+        dll_item_t *next = list->next;
+        free(list);
+        list = next;
+    }
+
+    return 0;
+}
