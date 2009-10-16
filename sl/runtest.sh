6f28a785353a7586ab27f005e2a83029bb8cbc6d
Kamil Dudka <kdudka@redhat.com>
2009-10-16 14:00:33 +0200

    sl: license as GPLv3

--- sl/runtest.sh
+++ sl/runtest.sh
@@ -2,20 +2,20 @@
 #
 # Copyright (C) 2009 Kamil Dudka <kdudka@redhat.com>
 #
-# This file is part of cgt (Call Graph Tools).
+# This file is part of sl.
 #
-# cgt is free software: you can redistribute it and/or modify
+# sl is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # any later version.
 #
-# cgt is distributed in the hope that it will be useful,
+# sl is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with cgt.  If not, see <http://www.gnu.org/licenses/>.
+# along with sl.  If not, see <http://www.gnu.org/licenses/>.
 
 VALGRIND=valgrind
 VALGRIND_EC=3
