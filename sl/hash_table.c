4964df6427b8318d8b8a174949351a18b3769ce7
Kamil Dudka <kdudka@redhat.com>
2009-10-27 22:10:50 +0100

    hash_table: ad-hoc hash table implementation...
    ... not used for now and not tested at all

---
+++ sl/hash_table.c
@@ -0,0 +1,160 @@
+/*
+ * Copyright (C) 2009 Kamil Dudka <kdudka@redhat.com>
+ *
+ * This file is part of sl.
+ *
+ * sl is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * sl is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with sl.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "hash_table.h"
+
+#include <assert.h>
+#include <stdlib.h>
+
+#define HT_NEW(type) \
+    (type *) malloc(sizeof(type))
+
+struct ht_hash_item {
+    void                    *data;
+    struct ht_hash_item     *next;
+};
+
+struct ht_hash_slot {
+    struct ht_hash_item     *head;
+    struct ht_hash_item     **tail;
+};
+
+struct ht_hash_table {
+    ht_hash_fnc_t           hash_fnc;
+    ht_eq_fnc_t             eq_fnc;
+    ht_free_fnc_t           free_fnc;
+    size_t                  size;
+    struct ht_hash_slot     *table;
+};
+
+static void ht_dummy_free_fnc(void *ptr)
+{
+    (void) ptr;
+}
+
+struct ht_hash_table* ht_create(
+        size_t              htsize,
+        ht_hash_fnc_t       hash_fnc,
+        ht_eq_fnc_t         eq_fnc,
+        ht_free_fnc_t       free_fnc)
+{
+    unsigned i;
+    struct ht_hash_table *ht = HT_NEW(struct ht_hash_table);
+    if (!ht)
+        // out of memory
+        return NULL;
+
+    // initialize hash table
+    ht->hash_fnc            = hash_fnc;
+    ht->eq_fnc              = eq_fnc;
+    ht->free_fnc            = (free_fnc)
+                                ? free_fnc
+                                : ht_dummy_free_fnc;
+    ht->size                = htsize;
+    ht->table               = (struct ht_hash_slot *)
+        calloc(htsize, sizeof(struct ht_hash_slot));
+
+    if (!ht->table) {
+        // out of memory
+        free(ht);
+        return NULL;
+    }
+
+    // initialize all slots
+    for(i = 0; i < htsize; ++i) {
+        struct ht_hash_slot *slot = ht->table + i;
+        slot->tail = &slot->head;
+    }
+
+    return ht;
+}
+
+void ht_destroy(struct ht_hash_table *ht)
+{
+    unsigned i;
+    assert(ht && ht->table);
+
+    // free all data managed by hash table
+    for (i = 0; i < ht->size; ++i) {
+        struct ht_hash_item *item = ht->table[i].head;
+        while (item) {
+            struct ht_hash_item *next = item->next;
+            ht->free_fnc(item->data);
+            free(item);
+            item = next;
+        }
+    }
+
+    // free hash table itself
+    free(ht->table);
+    free(ht);
+}
+
+static struct ht_hash_slot* ht_get_slot(struct ht_hash_table *ht, void *what)
+{
+    ht_hash_t hash = ht->hash_fnc(what);
+    unsigned idx = hash % ht->size;
+    return ht->table + idx;
+}
+
+static struct ht_hash_item* ht_seek_item(struct ht_hash_table *ht,
+                                         struct ht_hash_item *head, void *what)
+{
+    struct ht_hash_item *item = head;
+    for(; item; item = item->next)
+        if (ht->eq_fnc(item->data, what))
+            return item;
+
+    // not found
+    return NULL;
+}
+
+void* ht_lookup(struct ht_hash_table *ht, void *what)
+{
+    struct ht_hash_slot *slot = ht_get_slot(ht, what);
+    struct ht_hash_item *item = ht_seek_item(ht, slot->head, what);
+
+    return (item)
+        ? item->data
+        : NULL;
+}
+
+void* ht_insert_if_needed(struct ht_hash_table *ht, void *what)
+{
+    struct ht_hash_slot *slot = ht_get_slot(ht, what);
+    struct ht_hash_item *item = ht_seek_item(ht, slot->head, what);
+    if (item)
+        // item already hashed, return the original one
+        return item->data;
+
+    item = HT_NEW(struct ht_hash_item);
+    if (!item)
+        // out of memory
+        return NULL;
+
+    // initialize ht item
+    item->data = what;
+    item->next = NULL;
+
+    // append the item at end of the list
+    *slot->tail = item;
+    slot->tail = &item->next;
+
+    return what;
+}
---
+++ sl/hash_table.h
@@ -0,0 +1,115 @@
+/*
+ * Copyright (C) 2009 Kamil Dudka <kdudka@redhat.com>
+ *
+ * This file is part of sl.
+ *
+ * sl is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * sl is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with sl.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef H_GUARD_HASH_TABLE_H
+#define H_GUARD_HASH_TABLE_H
+
+#include <stdbool.h>
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * type used for hash value
+ *
+ * It is guaranteed to be an integral (signed or unsigned) type.
+ */
+typedef unsigned ht_hash_t;
+
+/**
+ * type of function evaluating hash value for a given (hashable) item
+ */
+typedef ht_hash_t (*ht_hash_fnc_t)(void *item);
+
+/**
+ * type of function comparing two (hashable) items for equality
+ *
+ * @param i1 The first item for comparison.
+ * @param i2 The second item for comparison.
+ * @return Return true if the given items are equal, false otherwise.
+ */
+typedef bool (*ht_eq_fnc_t)(void *i1, void *i2);
+
+/**
+ * type of function used to free a hashed item
+ */
+typedef void (*ht_free_fnc_t)(void *item);
+
+/**
+ * data internally used by hash table implementation
+ *
+ * You only need to keep pointer to this structure.
+ */
+struct ht_hash_table;
+
+/**
+ * create (empty) hash table
+ *
+ * @param htsize Size of hash table; will stay unchanged for the whole hash
+ * table life time.
+ * @param hash_fnc A function used to compute hash value for a given item.
+ * @param eq_fnc A function used to compare given items for equality.
+ * @param free_fnc A function used to free hashed item (for now only on hash
+ * table destruction). If NULL is given as free_fnc, the hashed items will not
+ * be freed.
+ *
+ * @return Return an initialized hash table object, NULL if memory is exhausted.
+ */
+struct ht_hash_table* ht_create(
+        size_t              htsize,
+        ht_hash_fnc_t       hash_fnc,
+        ht_eq_fnc_t         eq_fnc,
+        ht_free_fnc_t       free_fnc);
+
+/**
+ * destroy hash table
+ *
+ * This also calls a formerly given free_fnc function for all hashed items.
+ *
+ * @param ht Pointer to hash table internal data.
+ */
+void ht_destroy(struct ht_hash_table *ht);
+
+/**
+ * look for a given item within the hash table
+ *
+ * @param ht Pointer to hash table internal data.
+ * @param item An item (or its template) to look for.
+ * @return Returns the found item if any, NULL otherwise.
+ */
+void* ht_lookup(struct ht_hash_table *ht, void *item);
+
+/**
+ * insert an item into the hash table if the item is not there already
+ *
+ * @param ht Pointer to hash table internal data.
+ * @param item Item to look for and then eventually insert into hash table.
+ * @param Returns the found item if it was already hashed. If not, it tries to
+ * insert the item into the hash table and return the new item. It may also
+ * return NULL, if the memory is exhausted.
+ */
+void* ht_insert_if_needed(struct ht_hash_table *ht, void *item);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* H_GUARD_HASH_TABLE_H */
