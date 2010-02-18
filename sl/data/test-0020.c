ed0b2d5a7db571e8737522b6a8b444cecd7f1713
Kamil Dudka <kdudka@redhat.com>
2010-02-18 23:33:52 +0100

    test-0020: a new test case, failing for now

---
+++ sl/data/test-0020.c
@@ -0,0 +1,39 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+// return VAL_UNKNOWN of type CL_TYPE_BOOL while running symbolic execution
+// NOTE: executing this function on bare metal is usually bad idea
+bool synthesize_unknown_bool_value(void)
+{
+    return synthesize_unknown_bool_value();
+}
+
+// reaching this function indicates we are on a wrong way
+void trigger_null_dereference(void)
+{
+    void **ptr = NULL;
+    ptr = (void **) *ptr;
+}
+
+void test__cond_reasoning__with_zero_lookback_level() {
+    // obtain unknown value
+    const bool who_knows = synthesize_unknown_bool_value();
+
+    // who_knows is UNKNOWN at this point
+    if (who_knows) {
+
+        // who_knows should be seen as TRUE at this point, fail otherwise
+        if (!who_knows)
+            trigger_null_dereference();
+    } else {
+
+        // who_knows should be seen as FALSE at this point, fail otherwise
+        if (who_knows)
+            trigger_null_dereference();
+    }
+}
+
+int main() {
+    test__cond_reasoning__with_zero_lookback_level();
+    return 0;
+}
