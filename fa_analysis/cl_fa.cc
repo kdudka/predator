54e04e16a083afb0a444791db5a48d0f8dd27614
Kamil Dudka <kdudka@redhat.com>
2010-03-31 11:02:32 +0200

    fa_analysis: a new place for Jiri Simacek's pet project

---
+++ fa_analysis/cl_fa.cc
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2010 Jiri Simacek
+ *
+ * This file is part of predator.
+ *
+ * predator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * predator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with predator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cl/easy.hh>
+#include <cl/cl_msg.hh>
+
+// required by the gcc plug-in API
+extern "C" { int plugin_is_GPL_compatible; }
+
+void clEasyRun(CodeStorage::Storage &, const char *) {
+    CL_ERROR("fa_analysis is not implemented yet");
+}
