87b9c363e6336f1c02c4c098b12eb76335213a0d
Jiri Simacek <isimacek@fit.vutbr.cz>
2010-06-16 13:59:21 +0200

    copyrights in fa_analysis fixed

--- fa_analysis/treeaut.hh
+++ fa_analysis/treeaut.hh
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2010 Jiri Simacek
+ *
+ * This file is part of predator.
+ *
+ * predator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * predator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with predator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef TREEAUT_H
 #define TREEAUT_H
 
@@ -285,14 +304,14 @@ public:
 		this->transitions.clear();
 		this->finalStates.clear();
 	}
-
+/*
 	void loadFromDFS(const TA<T>::dfs_cache_type& dfsCache, const TA<T>& ta, const vector<size_t>& stack, bool registerFinalStates = true) {
-		for (TA<T>::dfs_iterator i = ta.dfsStart(dfsCache, stack); i->isValid(); ++i)
+		for (TA<T>::dfs_iterator i = ta.dfsStart(dfsCache, stack); i.isValid(); i.next())
 			this->addTransition(*i);
 		if (registerFinalStates)
 			this->addFinalStates(stack);
 	}
-	
+*/	
 	size_t newState() { return this->next_state++; }
 	
 	void updateStateCounter() {
