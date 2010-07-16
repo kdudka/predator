cb6b9e4431336057e89c24ee776cc92031ddf701
Kamil Dudka <kdudka@redhat.com>
2010-07-16 17:17:16 +0000

    symabstract: get ready for DLSs of DLSs
    test-0072 shows how it works...

---
+++ sl/data/test-0072.c
@@ -0,0 +1,141 @@
+#include "../sl.h"
+#include <stdlib.h>
+
+struct slave_item {
+    struct slave_item       *next;
+    struct slave_item       *prev;
+};
+
+struct slave_item* alloc_or_die_slave(void)
+{
+    struct slave_item *ptr = malloc(sizeof(*ptr));
+    if (!ptr)
+        abort();
+
+    ptr->next = NULL;
+    ptr->prev = NULL;
+    return ptr;
+}
+
+struct master_item {
+    struct master_item      *next;
+    struct master_item      *prev;
+    struct slave_item       *slave;
+};
+
+struct master_item* alloc_or_die_master(void)
+{
+    struct master_item *ptr = malloc(sizeof(*ptr));
+    if (!ptr)
+        abort();
+
+    ptr->next = NULL;
+    ptr->prev = NULL;
+    ptr->slave = NULL;
+    return ptr;
+}
+
+void dll_insert_slave(struct slave_item **dll)
+{
+    struct slave_item *item = alloc_or_die_slave();
+    struct slave_item *next = *dll;
+    item->next = next;
+    if (next)
+        next->prev = item;
+
+    *dll = item;
+}
+
+void* dll_create_generic(void (*insert_fnc)())
+{
+    void *dll = NULL;
+    insert_fnc(&dll);
+    insert_fnc(&dll);
+    insert_fnc(&dll);
+
+    // NOTE: running this on bare metal may cause the machine to swap a bit
+    int i;
+    for (i = 1; i; ++i)
+        insert_fnc(&dll);
+
+    return dll;
+}
+
+struct slave_item* dll_create_slave(void)
+{
+    return dll_create_generic(dll_insert_slave);
+}
+
+void dll_destroy_slave(struct slave_item *dll)
+{
+    // rewind
+    struct slave_item *prev;
+    while (dll && (prev = dll->prev))
+        dll = prev;
+
+    while (dll) {
+        struct slave_item *next = dll->next;
+        free(dll);
+        dll = next;
+    }
+}
+
+void dll_destroy_nested_lists(struct master_item *dll)
+{
+    // rewind
+    struct master_item *prev;
+    while (dll && (prev = dll->prev))
+        dll = prev;
+
+    while (dll) {
+        dll_destroy_slave(dll->slave);
+        dll->slave = NULL;
+        dll = dll->next;
+    }
+}
+
+void dll_destroy_master(struct master_item *dll)
+{
+    // rewind
+    struct master_item *prev;
+    while (dll && (prev = dll->prev))
+        dll = prev;
+
+    while (dll) {
+        struct master_item *next = dll->next;
+        free(dll);
+        dll = next;
+    }
+}
+
+void dll_insert_master(struct master_item **dll)
+{
+    struct master_item *item = alloc_or_die_master();
+    struct master_item *next = *dll;
+    item->next = next;
+    if (next)
+        next->prev = item;
+
+    item->slave = dll_create_slave();
+    *dll = item;
+}
+
+struct master_item* dll_create_master(void)
+{
+    return dll_create_generic(dll_insert_master);
+}
+
+int main()
+{
+    struct master_item *dll = dll_create_master();
+    ___sl_plot_by_ptr(dll, "01");
+
+    dll_destroy_nested_lists(dll);
+    ___sl_plot_by_ptr(dll, "02");
+
+    // just silence the gc
+    dll_destroy_master(dll);
+    ___sl_plot("03");
+
+    return 0;
+}
