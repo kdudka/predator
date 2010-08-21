kdudka/predator

commit d36b826c4e6a0d2ac253e2df43182d5e38a7f9cd

sl: eliminate some 'goto' statements

sl/symplot.cc
@@ -475,10 +475,7 @@ void SymPlot::Private::plotSingleValue(TValueId value) {
             this->workList.schedule(peer);
 
         bool eq;
-        if (!this->heap->proveEq(&eq, value, peer))
-            goto unhandled_pred;
-
-        if (!eq) {
+        if (this->heap->proveEq(&eq, value, peer) && eq) {
             if (VAL_NULL == peer) {
                 // 'value' is said to be non-zero
                 this->plotNeqZero(value);
@@ -491,7 +488,6 @@ void SymPlot::Private::plotSingleValue(TValueId value) {
             }
         }
 
-unhandled_pred:
         CL_WARN("SymPlot: unhandled predicate over values #"
                 << value << " and #" << peer);
     }
@@ -891,19 +887,17 @@ void SymPlot::Private::plotObj(TObjId obj) {
         // we got a bare value, which can't be followed, so we're done
         return;
 
-    if (OBJ_INVALID != this->heap->valGetCompositeObj(value)) {
+    if (OBJ_INVALID == this->heap->valGetCompositeObj(value)) {
+        // connect the variable node with its value
+        this->plotEdgeValueOf(obj, value);
+
+        // dig the target value recursively and plot (if not already)
+        this->workList.schedule(value);
+    }
+    else
         // dig composite object and eventually schedule the values inside
         this->digObj(obj);
-        goto wl_ready;
-    }
 
-    // connect the variable node with its value
-    this->plotEdgeValueOf(obj, value);
-
-    // dig the target value recursively and plot (if not already)
-    this->workList.schedule(value);
-
-wl_ready:
     this->digValues();
 }
 

sl/symheap.cc
@@ -1825,20 +1825,19 @@ void SymHeap::neqOp(ENeqOp op, TValueId valA, TValueId valB) {
     if (NEQ_ADD == op && haveDlSegAt(*this, valA, valB)) {
         // adding the 2+ flag implies adding of the 1+ flag
         this->dlSegCrossNeqOp(op, valA);
-        goto fallback;
+    }
+    else {
+        if (haveDlSeg(*this, valA, valB)) {
+            this->dlSegCrossNeqOp(op, valA);
+            return;
+        }
+
+        if (haveDlSeg(*this, valB, valA)) {
+            this->dlSegCrossNeqOp(op, valB);
+            return;
+        }
     }
 
-    if (haveDlSeg(*this, valA, valB)) {
-        this->dlSegCrossNeqOp(op, valA);
-        return;
-    }
-
-    if (haveDlSeg(*this, valB, valA)) {
-        this->dlSegCrossNeqOp(op, valB);
-        return;
-    }
-
-fallback:
     SymHeapTyped::neqOp(op, valA, valB);
 }
 

commit 27d010f229898d8ca115525ffe9b3a58475c8333

symplot: fix a bug introduced in d36b826

sl/symplot.cc
@@ -475,7 +475,7 @@ void SymPlot::Private::plotSingleValue(TValueId value) {
             this->workList.schedule(peer);
 
         bool eq;
-        if (this->heap->proveEq(&eq, value, peer) && eq) {
+        if (this->heap->proveEq(&eq, value, peer) && !eq) {
             if (VAL_NULL == peer) {
                 // 'value' is said to be non-zero
                 this->plotNeqZero(value);