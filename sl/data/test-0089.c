190c9acbd537910754513475dfccac293582e481
Kamil Dudka <kdudka@redhat.com>
2010-08-26 00:18:56 +0200

    test-0089: off-values, address aliasing, etc.

--- sl/data/test-0089.c
+++ sl/data/test-0089.c
@@ -1,4 +1,7 @@
 #include "../sl.h"
+#include <linux/stddef.h>
+#include <stdbool.h>
+#include <stddef.h>
 #include <stdlib.h>
 
 struct inner {
@@ -35,9 +38,93 @@ static struct outer* create(void)
     return data;
 }
 
+#define ROOT(type, field, addr) \
+    (type *) ((char *)addr - offsetof(type, field))
+
+#define CHK_CORE(neg, e1, e2) do {                      \
+    struct outer *_data = create();                     \
+    free(_data);                                        \
+    const bool eq = ((void *)(e1) == (void *)(e2));     \
+    if (neg == eq)                                      \
+        /* double free */                               \
+        free(_data);                                    \
+} while (0)
+
+#define CHK_EQ(e1, e2) CHK_CORE(false, e1, e2)
+#define CHK_NEQ(e1, e2) CHK_CORE(true, e1, e2)
+
+#define TRY_FREE_BY(item) do {                          \
+    struct outer *_data = create();                     \
+    void *_ptr = &_data->item;                          \
+    free(_ptr);                                         \
+} while (0)
+
+void chk_offval_handling()
+{
+    struct middle *ptr = malloc(sizeof *ptr);
+    if (!ptr)
+        abort();
+
+    struct middle *same_ptr = ROOT(struct middle, i1, &ptr->i1);
+
+    struct outer *d0 = ROOT(struct outer, m0, ptr);
+    struct outer *d1 = ROOT(struct outer, m1, ptr);
+    struct outer *same_d1 = ROOT(struct outer, m1, same_ptr);
+    void *x = &d1->m1;
+    ___sl_plot("test-0089-01");
+    CHK_EQ(ptr, d0);
+
+    // TODO: we should be able to deduce this
+    CHK_NEQ(d0, same_d1);
+
+    // this works
+    CHK_EQ(ptr, x);
+
+    // TODO: we should be able to deduce this, as (x == &d1->m1)
+    CHK_EQ(ptr, &d1->m1);
+
+    // this should be detected as invalid free()
+    free(d1);
+
+    // this should be OK
+    free(d0);
+
+    // this should be detected as double free()
+    free(ptr);
+}
+
 int main()
 {
     struct outer *data = create();
-    ___sl_plot_by_ptr(&data, "test-0089");
+    ___sl_plot_by_ptr(&data, "test-0089-00");
+
+    // ==
+    CHK_EQ(data,        &data->m0);
+    CHK_EQ(data,        &data->m0.i0);
+    CHK_EQ(data,        &data->m0.i0.p0);
+    CHK_EQ(&data->m1,   data->m1.i0.p0);
+
+    // !=
+    CHK_NEQ(data,       &data->m1);
+    CHK_NEQ(data,       &data->m0.i1);
+    CHK_NEQ(data,       data->m1.i0.p0);
+    CHK_NEQ(&data->m1,  &data->m0.i0.p0);
+
+    // avoid junk
+    free(data);
+
+    // this should be OK
+    TRY_FREE_BY(m0);
+    TRY_FREE_BY(m0.i0);
+    TRY_FREE_BY(m0.i0.p0);
+
+    // this shoould be detected as invalid free
+    TRY_FREE_BY(m1);
+    TRY_FREE_BY(m0.i1);
+    TRY_FREE_BY(m0.i0.p1);
+
+    // now some off-values play
+    chk_offval_handling();
+
     return EXIT_SUCCESS;
 }
