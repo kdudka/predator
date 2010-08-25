40b0da0b38876585f7ad143598541cc0a784d1de
Kamil Dudka <kdudka@redhat.com>
2010-08-25 23:09:02 +0200

    clutil: new guide for a visitor - traverseTypeIc()

--- sl/clutil.hh
+++ sl/clutil.hh
@@ -30,6 +30,8 @@
 
 #include <cl/code_listener.h>
 
+#include <stack>
+
 const struct cl_type* targetTypeOfPtr(const struct cl_type *clt);
 
 bool seekRefAccessor(const struct cl_accessor *ac);
@@ -37,5 +39,71 @@ bool seekRefAccessor(const struct cl_accessor *ac);
 int intCstFromOperand(const struct cl_operand *op);
 
 int varIdFromOperand(const struct cl_operand *op, const char **pName = 0);
+
+// TODO: define TFieldIdxChain within CodeStorage to avoid the template arg
+template <class TFieldIdxChain>
+struct CltStackItem {
+    const struct cl_type    *clt;
+    TFieldIdxChain          ic;
+};
+
+// take the given visitor through a composite type (or whatever you pass in)
+template <class TFieldIdxChain, class TVisitor>
+bool /* complete */ traverseTypeIc(const struct cl_type *clt, TVisitor &visitor,
+                                   bool digOnlyStructs = false)
+{
+    SE_BREAK_IF(!clt);
+
+    // initialize DFS
+    typedef CltStackItem<TFieldIdxChain> TItem;
+    TItem si;
+    si.clt = clt;
+    si.ic.push_back(0);
+
+    // DFS loop
+    std::stack<TItem> todo;
+    todo.push(si);
+    while (!todo.empty()) {
+        TItem &si = todo.top();
+        SE_BREAK_IF(si.ic.empty());
+
+        typename TFieldIdxChain::reference &nth = si.ic.back();
+        if (nth == si.clt->item_cnt) {
+            // done at this level
+            todo.pop();
+            continue;
+        }
+
+        if (digOnlyStructs && CL_TYPE_STRUCT != si.clt->code) {
+            // caller is interested only in CL_TYPE_STRUCT, skip this
+            ++nth;
+            continue;
+        }
+
+        const struct cl_type_item *item = si.clt->items + nth;
+        const TFieldIdxChain &icConst = si.ic;
+        if (!/* continue */visitor(icConst, item))
+            return false;
+
+        if (!item->type->item_cnt) {
+            // non-coposite type item
+            ++nth;
+            continue;
+        }
+
+        // nest into sub-type
+        TItem next;
+        next.clt = item->type;
+        next.ic = si.ic;
+        next.ic.push_back(0);
+        todo.push(next);
+
+        // move to the next at this level
+        ++nth;
+    }
+
+    // the traversal is done, without any interruption by visitor
+    return true;
+}
 
 #endif /* H_GUARD_CLUTIL_H */
