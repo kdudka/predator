kdudka/predator

commit 8358ce3b9317b2829aa42d6306f52d6a0554f3bb

symplot: fix a bug in traversing of composite objs

sl/symplot.cc
@@ -24,6 +24,7 @@
 #include <cl/location.hh>
 #include <cl/storage.hh>
 
+#include "clutil.hh"
 #include "symbt.hh"
 #include "symheap.hh"
 #include "symseg.hh"
@@ -743,65 +744,82 @@ bool SymPlot::Private::resolvePointsTo(TObjId *pDst, TValueId value) {
     }
 }
 
-void SymPlot::Private::digObjCore(TObjId obj) {
-    typedef std::pair<TObjId, bool /* last */> TStackItem;
-    std::stack<TStackItem> todo;
-    push(todo, obj, false);
-    while (!todo.empty()) {
-        bool last;
-        boost::tie(obj, last) = todo.top();
-        todo.pop();
+class ObjectDigger {
+    private:
+        SymPlot::Private    *const self_;
+        const TObjId        root_;
+        unsigned            level_;
 
-        const struct cl_type *clt = this->heap->objType(obj);
-        SE_BREAK_IF(!clt);
+    public:
+        ObjectDigger(SymPlot::Private *self, TObjId root):
+            self_(self),
+            root_(root),
+            level_(0)
+        {
+            const struct cl_type *clt = self_->heap->objType(root);
+            this->operate(TFieldIdxChain(), clt);
+        }
 
-        const enum cl_type_e code = clt->code;
-        switch (code) {
-            case CL_TYPE_ARRAY:
-            case CL_TYPE_CHAR:
-            case CL_TYPE_INT:
-            case CL_TYPE_PTR: {
-                this->plotSingleObj(obj);
-                TValueId value;
-                if (this->resolveValueOf(&value, obj)) {
-                    this->gobbleEdgeValueOf(obj, value);
-                    this->workList.schedule(value);
-                }
-                break;
-            }
+        ~ObjectDigger() {
+            // finally close all pending clusters
+            for(; 0 < level_; --level_)
+                self_->dotStream << "}" << std::endl;
+        }
 
-            case CL_TYPE_STRUCT:
+        bool operator()(TFieldIdxChain ic, const struct cl_type_item *item) {
+            this->operate(ic, item->type);
+            return /* continue */ true;
+        }
+
+        void operate(TFieldIdxChain ic, const struct cl_type *clt) {
+            SE_BREAK_IF(!clt);
+
+            const SymHeap &sh = *self_->heap;
+            const TObjId obj = subObjByChain(sh, root_, ic);
+            SE_BREAK_IF(obj <= 0);
+
+            // first close all pending clusters
+            for(; ic.size() < level_; --level_)
+                self_->dotStream << "}" << std::endl;
+            level_ = ic.size();
+
+            if (CL_TYPE_STRUCT == clt->code) {
                 // avoid duplicates
-                this->objDone.insert(obj);
+                self_->objDone.insert(obj);
 
-                this->digNext(obj);
-                this->openCluster(obj);
-                this->plotSingleObj(obj);
+                self_->digNext(obj);
+                self_->openCluster(obj);
+                self_->plotSingleObj(obj);
                 for (int i = 0; i < clt->item_cnt; ++i) {
-                    const TObjId sub = this->heap->subObj(obj, i);
-                    const struct cl_type *subClt = this->heap->objType(sub);
+                    const TObjId sub = self_->heap->subObj(obj, i);
+                    const struct cl_type *subClt = self_->heap->objType(sub);
                     if (!subClt || (subClt->code == CL_TYPE_STRUCT
                                 && !subClt->item_cnt))
                         // skip empty structures
                         continue;
 
-                    if (!hasKey(this->objDone, sub))
-                        this->plotEdgeSub(obj, sub);
-
-                    push(todo, sub, /* last */ (0 == i));
+                    if (!hasKey(self_->objDone, sub))
+                        self_->plotEdgeSub(obj, sub);
                 }
-                break;
-
-            default:
-                CL_DEBUG_MSG(this->lw, "SymPlot::Private::digObj(" << obj
-                        << "): Unimplemented type: " << code);
-                SE_TRAP;
+            }
+            else {
+                self_->plotSingleObj(obj);
+                TValueId value;
+                if (self_->resolveValueOf(&value, obj)) {
+                    self_->gobbleEdgeValueOf(obj, value);
+                    self_->workList.schedule(value);
+                }
+            }
         }
+};
 
-        if (last)
-            // we are done with the current cluster, close it now
-            this->dotStream << "}" << std::endl;
-    }
+void SymPlot::Private::digObjCore(TObjId obj) {
+    if (hasKey(this->objDone, obj))
+        return;
+
+    ObjectDigger visitor(this, obj);
+    const struct cl_type *clt = this->heap->objType(obj);
+    traverseTypeIc<TFieldIdxChain>(clt, visitor, /* digOnlyStructs */ true);
 }
 
 void SymPlot::Private::digObj(TObjId obj) {

sl/symdump.cc
@@ -22,6 +22,7 @@
 
 #include <cl/code_listener.h>
 
+#include "clutil.hh"
 #include "symheap.hh"
 #include "symutil.hh"
 
@@ -64,65 +65,38 @@ void dump_clt_core(const struct cl_type *clt) {
 
 } // namespace
 
-struct CltStackItem {
-    const struct cl_type    *clt;
-    const char              *name;
-    bool                    last;
-};
+bool dump_clt_visitor(TFieldIdxChain ic, const struct cl_type_item *item) {
+    // indent regarding the current nest level
+    const unsigned nestLevel = ic.size();
+    const std::string indent(nestLevel << 2, ' ');
+    cout << indent;
+
+    // print field name if any
+    const char *name = item->name;
+    if (name)
+        cout << "." << name << " = ";
+
+    // print type at the current level
+    const struct cl_type *clt = item->type;
+    SE_BREAK_IF(!clt);
+    dump_clt_core(clt);
+    cout << "\n";
+
+    return /* continue */ true;
+}
+
 void dump_clt(const struct cl_type *clt) {
     if (!clt) {
         cout << "NULL\n";
         return;
     }
 
-    CltStackItem item;
-    item.clt    = clt;
-    item.name   = 0;
-    item.last   = false;
+    // print type at the current level
+    dump_clt_core(clt);
+    cout << "\n";
 
-    // type stack
-    std::stack<CltStackItem> todo;
-    todo.push(item);
-
-    int nestLevel = 0;
-    while (!todo.empty()) {
-        item = todo.top();
-        todo.pop();
-
-        // indent regarding the current nest level
-        const std::string indent(nestLevel << 2, ' ');
-        cout << indent;
-
-        // print field name if any
-        const char *name = item.name;
-        if (name)
-            cout << "." << name << " = ";
-
-        // print type at the current level
-        clt = item.clt;
-        SE_BREAK_IF(!clt);
-        dump_clt_core(clt);
-        cout << "\n";
-
-        if (CL_TYPE_STRUCT == clt->code) {
-            // nest into struct
-            ++nestLevel;
-
-            // dig all sub-types
-            const int last = clt->item_cnt - 1;
-            for (int i = last; 0 <= i ; --i) {
-                item.clt    = clt->items[i].type;
-                item.name   = clt->items[i].name;
-                item.last   = (last == i);
-
-                todo.push(item);
-            }
-        }
-
-        // FIXME: this may misbehave under some circumstances
-        if (item.last)
-            --nestLevel;
-    }
+    // go through the type recursively
+    traverseTypeIc<TFieldIdxChain>(clt, dump_clt_visitor);
 }
 
 void dump_ac(const struct cl_accessor *ac) {

sl/symplot.hh
@@ -94,6 +94,9 @@ class SymPlot {
     private:
         struct Private;
         Private *d;
+
+        // internal helper of SymPlot::Private::digObjCore()
+        friend class ObjectDigger;
 };
 
 #endif /* H_GUARD_SYM_PLOT_H */

commit 16575c3d3405d518dec648b62b2b588cad753342

symplot: fix a bug introduced in 8358ce3
Simply speaking, the code was over-optimized and was completely
forgetting to plot internals of _some_ structures.  The bug was
reproducible with e.g. test-0077.

sl/symplot.cc
@@ -802,9 +802,6 @@ void ObjectDigger::operate(TFieldIdxChain ic, const struct cl_type *clt) {
 }
 
 void SymPlot::Private::digObjCore(TObjId obj) {
-    if (hasKey(this->objDone, obj))
-        return;
-
     ObjectDigger visitor(this, obj);
     const struct cl_type *clt = this->heap->objType(obj);
     traverseTypeIc<TFieldIdxChain>(clt, visitor, /* digOnlyStructs */ true);