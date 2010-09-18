7ebf0e374a1a047daf82fece0e2bdc1cfc63949f
Kamil Dudka <kdudka@redhat.com>
2010-09-18 15:19:33 +0200

    cld_unswitch: do not allocate struct cl_type on stack
    Static storage seems to be a better approach here, as type-info is not
    cloned by code listener and the on stack allocated object becomes
    invalid very quickly.

--- cl/cld_unswitch.cc
+++ cl/cld_unswitch.cc
@@ -141,8 +141,8 @@ void CldUnfoldSwitch::freeClonedSwitchSrc() {
 
 void CldUnfoldSwitch::emitCase(int cst, struct cl_type *type, const char *label)
 {
-    struct cl_type btype;
-    btype.uid                       = /* FIXME */ -1;
+    static struct cl_type btype;
+    btype.uid                       = /* FIXME */ 0x100000;
     btype.code                      = CL_TYPE_BOOL;
     btype.loc.file                  = 0;
     btype.loc.line                  = -1;
