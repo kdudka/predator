f0150cfe65cde949468462b6faa6ad3946fea6b3
Kamil Dudka <kdudka@redhat.com>
2010-11-27 13:28:00 +0100

    test-0119: simplification of test-0118 (demo)

---
+++ sl/data/test-0119.c
@@ -0,0 +1,41 @@
+#include "../sl.h"
+#include <stdlib.h>
+#define NEW(type) (type *) malloc(sizeof(type))
+
+struct item {
+    struct item *head;
+    struct item *next;
+};
+
+// create SLL of length 3 with head pointers
+static struct item* create_sll(void)
+{
+    struct item *head       = NEW(struct item);
+    head->head              = head;
+    head->next              = NEW(struct item);
+    head->next->head        = head;
+    head->next->next        = NEW(struct item);
+    head->next->next->head  = head;
+    head->next->next->next  = NULL;
+    return head;
+}
+
+// insert one node right after head and return its address
+static struct item* create_longer_sll(void)
+{
+    struct item *head       = create_sll();
+    struct item *next       = head->next;
+    head->next              = NEW(struct item);
+    head->next->head        = head;
+    head->next->next        = next;
+    return head->next;
+}
+
+// destroy head
+int main(void)
+{
+    struct item *sll = create_longer_sll();
+    free(sll->head);
+    ___sl_plot_by_ptr(&sll, NULL);
+    return 0;
+}
