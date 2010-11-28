9eaf51b894eae080fbe67acd143e25a659f37228
Kamil Dudka <kdudka@redhat.com>
2010-11-28 01:14:36 +0100

    test-0120: hlist_head/hlist_node based "list" of length one

---
+++ sl/data/test-0120.c
@@ -0,0 +1,66 @@
+#include "../sl.h"
+#include <stdlib.h>
+
+struct list_head {
+	struct list_head *next, *prev;
+};
+
+static inline void INIT_LIST_HEAD(struct list_head *list)
+{
+	list->next = list;
+	list->prev = list;
+}
+
+struct hlist_head {
+	struct hlist_node *first;
+};
+
+struct hlist_node {
+	struct hlist_node *next, **pprev;
+};
+
+#define HLIST_HEAD(name) struct hlist_head name = {  .first = NULL }
+
+static inline void INIT_HLIST_NODE(struct hlist_node *h)
+{
+	h->next = NULL;
+	h->pprev = NULL;
+}
+
+static inline int hlist_empty(const struct hlist_head *h)
+{
+	return !h->first;
+}
+
+static inline void hlist_add_head(struct hlist_node *n, struct hlist_head *h)
+{
+	struct hlist_node *first = h->first;
+	n->next = first;
+	if (first)
+		first->pprev = &n->next;
+	h->first = n;
+	n->pprev = &h->first;
+}
+
+struct my_hlist {
+    struct list_head nested;
+    struct hlist_node node;
+};
+
+int main() {
+    HLIST_HEAD(my_hlist_head);
+
+    struct my_hlist *item = malloc(sizeof *item);
+    if (!item)
+        return EXIT_FAILURE;
+
+    INIT_LIST_HEAD(&item->nested);
+    INIT_HLIST_NODE(&item->node);
+    hlist_add_head(&item->node, &my_hlist_head);
+    if (hlist_empty(&my_hlist_head))
+        abort();
+
+    ___sl_plot(NULL);
+    free(item);
+    return EXIT_SUCCESS;
+}
