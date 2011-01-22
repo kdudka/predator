7941ae535a1ae334472de84d5522c3f9a0e33636
Jiri Simacek <isimacek@fit.vutbr.cz>
2011-01-22 04:33:24 +0100

    fa: sorting examples

---
+++ fa/data/mergesort.c
@@ -0,0 +1,104 @@
+#include <stdlib.h>
+
+int __nondet();
+
+struct DItem {
+	struct DItem* next;
+	int value;
+};
+
+struct TLItem {
+	struct TLItem* next;
+	struct DItem* data;	
+};
+
+int main() {
+
+	struct TLItem* data = NULL;
+	struct DItem* item, * item2;
+	struct TLItem *lItem;
+
+	while (__nondet()) {
+
+          item = malloc(sizeof *item);
+          item->next = NULL;
+          item->value = __nondet();
+
+          lItem = malloc(sizeof *lItem);
+	  if (data) {
+            lItem->next = data->next;
+	    data->next = lItem;
+	  } else {
+	    lItem->next = lItem;
+	    data = lItem;
+	  }
+          lItem->data = item;
+
+	  item = NULL;
+	  lItem = NULL;
+
+        }
+	
+	if (!data)
+	  return 0;
+
+	while (data->next != data) {
+	  item = data->data;
+	  item2 = data->next->data;
+
+	  lItem = data->next;
+	  data->next = lItem->next;
+	  free(lItem);
+
+	  if (__nondet()) {  
+	    data->data = item;
+	    item = item->next;
+	  } else {
+	    data->data = item2;
+	    item2 = item2->next;
+	  }
+
+	  struct DItem* dst = data->data;
+
+	  while (item || item2) {
+
+	    if (!item2) {
+	      dst->next = item;
+	      item = NULL;
+	      break;
+	    }
+
+	    if (!item) {
+	      dst->next = item2;
+	      item2 = NULL;
+	      break;
+	    }
+
+	    if (__nondet()) {
+	      dst->next = item;
+	      item = item->next;
+	    } else {
+	      dst->next = item2;
+	      item2 = item2->next;
+	    }
+
+	    dst = dst->next;
+
+	  }
+
+	  data = data->next;
+
+	}
+
+	item = data->data;
+	free(data);
+
+	while (item) {
+	  item2 = item;
+	  item = item->next;
+	  free(item2);
+	}
+
+	return 0;
+
+}
---
+++ fa/data/sll-insertsort.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+
+int __nondet();
+
+int main() {
+
+    struct T {
+        struct T* next;
+    };
+
+    struct T* x = NULL;
+    struct T* y = NULL;
+
+    while (__nondet()) {
+        y = malloc(sizeof(*y));
+        y->next = x;
+        x = y;
+    }
+
+    struct T* sorted = NULL;
+    struct T* pred = NULL;
+    struct T* z = NULL;
+
+    while (x) {
+	y = x;
+	x = x->next;
+	pred = NULL;
+	z = sorted;
+
+	while (z && __nondet()) {
+		pred = z;
+		z = z->next;
+	}
+	
+	y->next = z;
+	if (pred) pred->next = y;
+	else sorted = y;
+    }
+
+    while (sorted != NULL) {
+        x = sorted;
+        sorted = sorted->next;
+        free(x);
+    }
+
+    return 0;
+
+}
