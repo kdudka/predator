780befd68787a164199de1a9f9cb94cddd1c7e1f
Jiri Simacek <isimacek@fit.vutbr.cz>
2011-01-31 19:20:51 +0100

    fa: release script modification

--- make-forester.sh
+++ make-forester.sh
@@ -10,30 +10,31 @@ BRANCH="`git status | head -1 | sed 's/^#.* //'`"     || die
 printf "%s: considering release of branch '%s' of '%s'...\n" \
     "$0" "$BRANCH" "$REPO"
 set -x
-test -z "`git diff HEAD`"                           || die
-test -z "`git diff origin/$BRANCH`"                 || die
+#test -z "`git diff HEAD`"                           || die
+#test -z "`git diff origin/$BRANCH`"                 || die
 make -j5 -C fa distcheck                            || die
 
 STAMP="`git log --pretty="%cd-%h" --date=short -1`" || die
 NAME="${PROJECT}-$STAMP"                            || die
 TMP="`mktemp -d`"                                   || die
 mkdir "${TMP}/${NAME}"                              || die
+mkdir "${TMP}/GIT"                              || die
+cd "${TMP}/GIT"                                 || die
+git init                                            || die
+git pull "$REPO" "$BRANCH"                          || die
 git log --pretty="format:%ad  %an%n%n%w(80,8,8)%B%n" --date=short -- fa > ${TMP}/${NAME}/ChangeLog || die
 cd "${TMP}/${NAME}"                                 || die
-cp "$REPO/CMakeLists.common" .				|| die
-cp "$REPO/COPYING" .				|| die
-cp "$REPO/FAQ" .				|| die
-cp "$REPO/HACKING" .				|| die
-cp "$REPO/Makefile" .				|| die
-cp "$REPO/README" .				|| die
-cp -r "$REPO/include" .					|| die 
-cp -r "$REPO/cl" .					|| die 
-cp "$REPO/fa/*.hh" ./fa/				|| die 
-cp "$REPO/fa/*.cc" ./fa/				|| die 
-cp "$REPO/fa/CMakeLists.txt" ./fa/			|| die 
-cp "$REPO/fa/data/sll-rev.c" ./fa/examples/		|| die 
-cp "$REPO/fa/data/sll-mergesort.c" ./fa/examples/	|| die 
-cp "$REPO/fa/data/dsw.c" ./fa/examples/			|| die 
+cp ${TMP}/GIT/fa_release/* .				|| die
+cp -r ${TMP}/GIT/include .					|| die 
+cp -r ${TMP}/GIT/cl .					|| die 
+mkdir ./fa					|| die
+cp ${TMP}/GIT/fa/*.hh ./fa/				|| die 
+cp ${TMP}/GIT/fa/*.cc ./fa/				|| die 
+cp ${TMP}/GIT/fa/CMakeLists.txt ./fa/			|| die 
+mkdir ./fa/examples				|| die
+cp ${TMP}/GIT/fa/data/sll-rev.c ./fa/examples/		|| die 
+cp ${TMP}/GIT/fa/data/sll-mergesort.c ./fa/examples/	|| die 
+cp ${TMP}/GIT/fa/data/dsw.c ./fa/examples/			|| die 
 cd ..                                               || die
 test -d "$NAME"                                     || die
 TGZ="${NAME}.tar.gz"                                || die
