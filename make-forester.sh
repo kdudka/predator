cb05fb50f6fc7abc880997f0e8b4385b9cd0bfdc
Jiri Simacek <isimacek@fit.vutbr.cz>
2011-01-31 17:05:34 +0100

    fa: minor fixes in release script

--- make-forester.sh
+++ make-forester.sh
@@ -5,19 +5,20 @@ die(){
 
 PROJECT="forester"
 REPO="`git rev-parse --show-toplevel`"              || die
-BRANCH="`git stat | head -1 | sed 's/^#.* //'`"     || die
+BRANCH="`git status | head -1 | sed 's/^#.* //'`"     || die
 
 printf "%s: considering release of branch '%s' of '%s'...\n" \
     "$0" "$BRANCH" "$REPO"
 set -x
 test -z "`git diff HEAD`"                           || die
 test -z "`git diff origin/$BRANCH`"                 || die
-make -j5 distcheck                                  || die
+make -j5 -C fa distcheck                            || die
 
 STAMP="`git log --pretty="%cd-%h" --date=short -1`" || die
 NAME="${PROJECT}-$STAMP"                            || die
 TMP="`mktemp -d`"                                   || die
 mkdir "${TMP}/${NAME}"                              || die
+git log --pretty="format:%ad  %an%n%n%w(80,8,8)%B%n" --date=short -- fa > ${TMP}/${NAME}/ChangeLog || die
 cd "${TMP}/${NAME}"                                 || die
 cp "$REPO/CMakeLists.common" .				|| die
 cp "$REPO/COPYING" .				|| die
@@ -33,7 +34,6 @@ cp "$REPO/fa/CMakeLists.txt" ./fa/			|| die
 cp "$REPO/fa/data/sll-rev.c" ./fa/examples/		|| die 
 cp "$REPO/fa/data/sll-mergesort.c" ./fa/examples/	|| die 
 cp "$REPO/fa/data/dsw.c" ./fa/examples/			|| die 
-make ChangeLog                                      || die
 cd ..                                               || die
 test -d "$NAME"                                     || die
 TGZ="${NAME}.tar.gz"                                || die
