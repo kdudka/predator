c76ec7f4e60ca65868b1d0736732acb0c6142db1
Kamil Dudka <kdudka@redhat.com>
2011-06-24 22:02:24 +0200

    symexec: perform abstraction on loop-closing CFG edges only

--- sl/config.h
+++ sl/config.h
@@ -88,7 +88,7 @@
 /**
  * if 1, do not perform abstraction on each end of BB, but only when looping
  */
-#define SE_ABSTRACT_ON_LOOP_EDGES_ONLY      0
+#define SE_ABSTRACT_ON_LOOP_EDGES_ONLY      1
 
 /**
  * if 1, perform abstraction after each just completed call on @b caller's side
