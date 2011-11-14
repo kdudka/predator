330553699d3fc5ac7a05dba941d92a53fa25fac3
Kamil Dudka <kdudka@redhat.com>
2011-11-14 18:42:22 +0100

    test-0196: a training example for the introduction of VT_RANGE

---
+++ tests/predator-regre/test-0196.c
@@ -0,0 +1,44 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <verifier-builtins.h>
+
+static void *zalloc(size_t nelem, size_t elsize)
+{
+    void *addr = calloc(nelem, elsize);
+    if (addr)
+        return addr;
+
+    // OOM
+    abort();
+}
+
+#define ZNEW_ARRAY(nelem, type)         ((type *) zalloc((nelem), sizeof(type)))
+#define ADVANCE_PTR_BY_OFF(ptr, off)    ((typeof(ptr)) ((char *)(ptr) + (off)))
+
+static void* create_shape(bool simple)
+{
+    void **root = NULL;
+    while (___sl_get_nondet_int()) {
+        void **node = ZNEW_ARRAY(2, void *);
+        void **next = ADVANCE_PTR_BY_OFF(node, sizeof(void *));
+        *next = root;
+        root  = node;
+
+        *node = (simple || ___sl_get_nondet_int())
+            ? next
+            : node;
+    }
+
+    return root;
+}
+
+int main()
+{
+    void *simple = create_shape(/* simple */ true);
+    ___sl_plot("01-simple", &simple);
+
+    void *tricky = create_shape(/* simple */ false);
+    ___sl_plot("02-tricky", &tricky);
+
+    return 0;
+}
