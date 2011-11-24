kdudka/predator

commit b54755a7988789d75fb308d41f66e2059897098d

sl: minor fixes and code cleanup

sl/memdebug.cc
@@ -88,7 +88,8 @@ struct AmountFormatter {
 
 std::ostream& operator<<(std::ostream &str, const AmountFormatter &fmt) {
     using namespace std;
-    str << std::fixed << setw(fmt.width) << setprecision(fmt.pre) << fmt.value;
+    str << fixed << setw(fmt.width) << setprecision(fmt.pre) << fmt.value
+        << resetiosflags;
     return str;
 }
 

sl/symheap.cc
@@ -420,6 +420,7 @@ class CustomValueMapper {
         TCustomByLong       numMap;
         TCustomByReal       fpnMap;
         TCustomByString     strMap;
+        TValId              inval_;
 
     public:
         RefCounter          refCnt;
@@ -431,6 +432,7 @@ class CustomValueMapper {
                 case CV_INVALID:
                 default:
                     CL_BREAK_IF("invalid call of CustomValueMapper::lookup()");
+                    return inval_ = VAL_INVALID;
 
                 case CV_FNC:
                     return assignInvalidIfNotFound(fncMap, item.data.uid);

sl/symabstract.cc
@@ -339,9 +339,6 @@ void abstractNonMatchingValues(
 
     if (OK_DLS == sh.valTargetKind(dstAt))
         dlSegSyncPeerData(sh, dstAt);
-
-    if (bidir && OK_DLS == sh.valTargetKind(srcAt))
-        dlSegSyncPeerData(sh, srcAt);
 }
 
 void clonePrototypes(SymHeap &sh, TValId addr, TValId dup) {

sl/symproc.cc
@@ -244,6 +244,7 @@ bool SymProc::checkForInvalidDeref(TValId val, const TOffset sizeOfTarget) {
     switch (code) {
         case VT_RANGE:
             CL_BREAK_IF("please implement");
+            // fall through!
 
         case VT_INVALID:
         case VT_COMPOSITE:
@@ -881,6 +882,7 @@ void SymExecCore::execFree(TValId val) {
 
         case VT_RANGE:
             CL_BREAK_IF("please implement");
+            // fall through!
 
         case VT_INVALID:
         case VT_ABSTRACT:

commit e7d7206b9735e0fc2e8b338830ac3d15f49c62db

sl: use std::resetiosflags correctly and avoid a SIGSEGV
in production in case Code Listener failed to capture a type of data
pointer.  This commit fixes a regression introduced in b54755a.

sl/memdebug.cc
@@ -89,7 +89,7 @@ struct AmountFormatter {
 std::ostream& operator<<(std::ostream &str, const AmountFormatter &fmt) {
     using namespace std;
     str << fixed << setw(fmt.width) << setprecision(fmt.pre) << fmt.value
-        << resetiosflags;
+        << resetiosflags(ios_base::showbase);
     return str;
 }
 

sl/symheap.cc
@@ -2262,7 +2262,11 @@ TObjId SymHeapCore::ptrAt(TValId at) {
 
     // generic pointer, (void *) if available
     const TObjType clt = stor_.types.genericDataPtr();
-    CL_BREAK_IF(!clt || clt->code != CL_TYPE_PTR);
+    if (!clt || clt->code != CL_TYPE_PTR) {
+        CL_BREAK_IF("Code Listener failed to capture a type of data pointer");
+        return OBJ_INVALID;
+    }
+
     const TOffset size = clt->size;
     CL_BREAK_IF(size <= 0);
 