kdudka/predator

commit ced8e4513a72715d55ead4d9674452d7bd4677a1

cl: remember to restore output stream's flags

cl/cldebug.cc
@@ -22,6 +22,8 @@
 #include <cl/clutil.hh>
 #include <cl/storage.hh>
 
+#include <iomanip>
+
 #include <boost/algorithm/string/replace.hpp>
 
 namespace {
@@ -187,7 +189,7 @@ void operandToStreamCstInt(std::ostream &str, const struct cl_operand &op) {
 
         case CL_TYPE_PTR:
             if (val)
-                str << "0x" << std::hex << val;
+                str << "0x" << std::hex << val << std::resetiosflags;
             else
                 str << "NULL";
 

cl/stopwatch.cc
@@ -46,7 +46,8 @@ float /* sec */ StopWatch::elapsed() const {
 }
 
 std::ostream& operator<<(std::ostream &str, const StopWatch &watch) {
+    using namespace std;
     const float elapsed = watch.elapsed();
-    str << std::fixed << std::setprecision(3) << elapsed << " s";
+    str << fixed << setprecision(3) << elapsed << " s" << resetiosflags;
     return str;
 }

commit e213f4d6aa3f0e19d0013d30c562e230136eddf5

cl: use std::resetiosflags correctly and dig more type-info
This commit fixes a regression introduced in ced8e45.

cl/cl_storage.cc
@@ -475,9 +475,12 @@ void ClStorageBuilder::Private::digOperand(const TOp *op, bool skipVarInit) {
 
     // read type of each array index in the chain
     const struct cl_accessor *ac = op->accessor;
-    for (; ac; ac = ac->next)
+    for (; ac; ac = ac->next) {
+        readTypeTree(typeDb, ac->type);
+
         if (ac->code == CL_ACCESSOR_DEREF_ARRAY)
             readTypeTree(typeDb, ac->data.array.index->type);
+    }
 
     enum cl_operand_e code = op->code;
     switch (code) {

cl/cldebug.cc
@@ -189,7 +189,8 @@ void operandToStreamCstInt(std::ostream &str, const struct cl_operand &op) {
 
         case CL_TYPE_PTR:
             if (val)
-                str << "0x" << std::hex << val << std::resetiosflags;
+                str << "0x" << std::hex << val
+                    << std::resetiosflags(std::ios_base::showbase);
             else
                 str << "NULL";
 

cl/stopwatch.cc
@@ -48,6 +48,7 @@ float /* sec */ StopWatch::elapsed() const {
 std::ostream& operator<<(std::ostream &str, const StopWatch &watch) {
     using namespace std;
     const float elapsed = watch.elapsed();
-    str << fixed << setprecision(3) << elapsed << " s" << resetiosflags;
+    str << fixed << setprecision(3) << elapsed << " s"
+        << resetiosflags(ios_base::showbase);
     return str;
 }