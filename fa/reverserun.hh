198e52d2c56ea4a4db2fdf8d1e4a03f98d34503d
Ondra Lengal <ondra.lengal@gmail.com>
2012-05-25 15:57:49 +0200

    reverserun: switch one boost's unordered_map to std's

--- fa/reverserun.hh
+++ fa/reverserun.hh
@@ -25,9 +25,6 @@
 #include <stdexcept>
 #include <algorithm>
 
-#include <boost/unordered_map.hpp>
-#include <boost/unordered_set.hpp>
-
 #include "treeaut.hh"
 #include "forestautext.hh"
 #include "abstractbox.hh"
