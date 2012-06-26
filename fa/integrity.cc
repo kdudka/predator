7bd3df830858c0a2632f7161fdc25d90e02539b2
Ondra Lengal <ondra.lengal@gmail.com>
2012-06-26 17:07:04 +0200

    integrity: move more methods to implementation file

--- fa/integrity.cc
+++ fa/integrity.cc
@@ -20,6 +20,82 @@
 // Forester headers
 #include "integrity.hh"
 
+bool Integrity::checkState(
+	const TreeAut& ta,
+	size_t state,
+	const std::set<size_t>& defined,
+	std::vector<bool>& bitmap,
+	std::map<std::pair<const TreeAut*, size_t>, std::set<size_t>>& states) const
+{
+	const Data* data;
+
+	if (this->fae.isData(state, data))
+	{
+		if (!data->isRef())
+			return true;
+
+		return this->checkRoot(data->d_ref.root, bitmap, states);
+	}
+
+	auto p = states.insert(std::make_pair(std::make_pair(&ta, state), defined));
+
+	if (!p.second)
+		return (defined == p.first->second);
+
+	for (TreeAut::iterator i = ta.begin(state); i != ta.end(state); ++i)
+	{
+		const TypeBox* typeBox = static_cast<const TypeBox*>(i->label()->boxLookup(static_cast<size_t>(-1), nullptr));
+
+		assert(typeBox);
+
+		const std::vector<size_t>& sels = typeBox->getSelectors();
+
+		std::set<size_t> tmp(sels.begin(), sels.end());
+
+		for (auto s : defined)
+		{
+			if (tmp.erase(s) != 1)
+				return false;
+		}
+
+		if (!i->label()->iterate(CheckIntegrityF(*this, ta, *i, &tmp, bitmap, states)))
+			return false;
+
+		if (!tmp.empty())
+			return false;
+	}
+
+	return true;
+}
+
+
+bool Integrity::checkRoot(
+	size_t root,
+	std::vector<bool>& bitmap,
+	std::map<std::pair<const TreeAut*, size_t>, std::set<size_t>>& states) const
+{
+	// Assertions
+	assert(root < this->fae.roots.size());
+	assert(this->fae.roots[root]);
+	assert(root < bitmap.size());
+
+	if (bitmap[root])
+		return true;
+
+	bitmap[root] = true;
+
+	std::set<size_t> tmp;
+
+	this->enumerateSelectorsAtLeaf(tmp, root);
+
+	for (auto s : this->fae.roots[root]->getFinalStates())
+	{
+		if (!this->checkState(*this->fae.roots[root], s, tmp, bitmap, states))
+			return false;
+	}
+
+	return true;
+}
 
 bool Integrity::check() const
 {
