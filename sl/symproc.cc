84dadcf812024665dcd85cd876f6276b7679764f
Kamil Dudka <kdudka@redhat.com>
2012-07-07 22:23:24 +0200

    sl/symproc: valMerge() can now remove two abstract paths
    ... at a time, which appears useful on treiber_stk-4_push_pop-restr.c

--- sl/symproc.cc
+++ sl/symproc.cc
@@ -1430,7 +1430,7 @@ bool spliceOutAbstractPathCore(
         SymProc                &proc,
         const TValId            beg,
         const TValId            endPoint,
-        const bool              readOnlyMode)
+        const bool              readOnlyMode = false)
 {
     SymHeap &sh = proc.sh();
 
@@ -1490,10 +1490,10 @@ bool spliceOutAbstractPathCore(
 }
 
 bool spliceOutAbstractPath(
-        SymState                    &dst,
         SymProc                     &proc,
         const TValId                 atAddr,
-        const TValId                 pointingTo)
+        const TValId                 pointingTo,
+        const bool                   readOnlyMode = false)
 {
     SymHeap &sh = proc.sh();
     const TValId seg = sh.valRoot(atAddr);
@@ -1501,9 +1501,10 @@ bool spliceOutAbstractPath(
 
     if (pointingTo == peer && peer != seg) {
         // assume identity over the two parts of a DLS
+        CL_BREAK_IF(readOnlyMode);
+
         dlSegReplaceByConcrete(sh, seg, peer);
         sh.traceUpdate(new Trace::SpliceOutNode(sh.traceNode(), OK_DLS));
-        dst.insert(sh);
         return true;
     }
 
@@ -1520,18 +1521,16 @@ bool spliceOutAbstractPath(
         // read-only attempt failed
         return false;
 
-    // splicing-out a single list segment is the most common case
-    if (!spliceOutAbstractPathCore(proc, seg, endPoint, /* RO */ false)) {
-        CL_BREAK_IF("failed to splice-out a single list segment");
-        return false;
-    }
+    if (readOnlyMode || spliceOutAbstractPathCore(proc, seg, endPoint))
+        return true;
 
-    dst.insert(sh);
-    return true;
+    CL_BREAK_IF("failed to splice-out a single list segment");
+    return false;
 }
 
 bool valMerge(SymState &dst, SymProc &proc, TValId v1, TValId v2) {
     SymHeap &sh = proc.sh();
+    const struct cl_loc *loc = proc.lw();
 
     // check that at least one value is unknown
     moveKnownValueToLeft(sh, v1, v2);
@@ -1549,15 +1548,48 @@ bool valMerge(SymState &dst, SymProc &proc, TValId v1, TValId v2) {
     // where did we get here from?
     Trace::Node *trNode = sh.traceNode();
 
-    if (VT_ABSTRACT == code1 && spliceOutAbstractPath(dst, proc, v1, v2))
+    if (VT_ABSTRACT == code1 && spliceOutAbstractPath(proc, v1, v2)) {
         // splice-out succeeded ... ls(v1, v2)
+        dst.insert(sh);
         return true;
+    }
 
-    if (VT_ABSTRACT == code2 && spliceOutAbstractPath(dst, proc, v2, v1))
+    if (VT_ABSTRACT == code2 && spliceOutAbstractPath(proc, v2, v1)) {
         // splice-out succeeded ... ls(v2, v1)
+        dst.insert(sh);
         return true;
+    }
 
-    CL_DEBUG("failed to splice-out list segment, has to over-approximate");
+    // still no luck, try to look through possibly empty objects!
+    const TValId v1Tail = lookThrough(sh, v1);
+    const TValId v2Tail = lookThrough(sh, v2);
+    if (v1Tail == v2Tail && v1 != v1Tail && v2 != v2Tail) {
+        const TValId tail = sh.valRoot(v1Tail);
+        if (VAL_NULL != tail && !objMinLength(sh, tail))
+            goto fail;
+
+        CL_DEBUG_MSG(loc, "valMerge() removes two abstract paths at a time");
+
+        if (!spliceOutAbstractPath(proc, v1, v1Tail, /* read-only */ true))
+            goto fail;
+        if (!spliceOutAbstractPath(proc, v2, v2Tail, /* read-only */ true))
+            goto fail;
+
+        // go ahead, try it read-write!
+
+        if (spliceOutAbstractPath(proc, v1, v1Tail)
+                && spliceOutAbstractPath(proc, v2, v2Tail))
+        {
+            dst.insert(sh);
+            return true;
+        }
+
+        CL_ERROR_MSG(loc, "internal error in valMerge(), heap inconsistent!");
+        CL_BREAK_IF("this will need some debugging...");
+    }
+
+fail:
+    CL_DEBUG_MSG(loc, "failed to splice-out list segment!");
     trNode = new Trace::SpliceOutNode(trNode, /* failed */ 0);
     sh.traceUpdate(trNode);
     dst.insert(sh);
