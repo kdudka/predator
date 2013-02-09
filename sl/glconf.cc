dd80fab30c07b8f7c80e76f2741054e8befbda8f
Kamil Dudka <kdudka@redhat.com>
2013-02-09 18:55:44 +0100

    sl/glconf: parse comma-separated list of options

--- sl/glconf.cc
+++ sl/glconf.cc
@@ -22,56 +22,121 @@
 
 #include <cl/cl_msg.hh>
 
-#include <cstring>                  // for strlen(), strncmp()
+#include <algorithm>
+#include <map>
+
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/foreach.hpp>
 
 namespace GlConf {
 
 Options data;
 
-void loadConfigString(const std::string &cnf)
+class ConfigStringParser {
+    public:
+        ConfigStringParser();
+        void handleRawOption(const std::string &) const;
+
+    private:
+        typedef void (*THandler)(const std::string &, const std::string &);
+        typedef std::map<std::string, THandler> TLookup;
+        TLookup tbl_;
+};
+
+using std::string;
+
+void assumeNoValue(const string &name, const string &value)
 {
-    using std::string;
+    if (value.empty())
+        return;
+
+    CL_WARN("option \"" << name << "\" takes no value");
+}
+
+void handleErrorLabel(const string &name, const string &value)
+{
+    if (value.empty()) {
+        CL_WARN("ignoring option \"" << name << "\" without a valid value");
+        return;
+    }
+
+    data.errLabel = value;
+}
+
+void handleNoErrorRecovery(const string &name, const string &value)
+{
+    assumeNoValue(name, value);
+    data.errorRecoveryMode = /* no_error_recovery */ 0;
+}
+
+void handleNoPlot(const string &name, const string &value)
+{
+    assumeNoValue(name, value);
+    data.skipUserPlots = true;
+}
+
+void handleOOM(const string &name, const string &value)
+{
+    assumeNoValue(name, value);
+    data.oomSimulation = true;
+}
+
+void handleTrackUninit(const string &name, const string &value)
+{
+    assumeNoValue(name, value);
+    data.trackUninit = true;
+}
+
+ConfigStringParser::ConfigStringParser()
+{
+    tbl_["error_label"]             = handleErrorLabel;
+    tbl_["no_error_recovery"]       = handleNoErrorRecovery;
+    tbl_["no_plot"]                 = handleNoPlot;
+    tbl_["oom"]                     = handleOOM;
+    tbl_["track_uninit"]            = handleTrackUninit;
+}
+
+void ConfigStringParser::handleRawOption(const string &raw) const
+{
+    // split the string to name/value, separated by ':'
+    string::const_iterator it = std::find(raw.begin(), raw.end(), ':');
+    const string name(raw.begin(), it);
+
+    // lookup by name
+    const TLookup::const_iterator hit = tbl_.find(name);
+    if (tbl_.end() == hit) {
+        CL_WARN("unknown option: " << name);
+        return;
+    }
+
+    if (raw.end() != it)
+        // skip ':' in the value part
+        ++it;
+
+    const string value(it, raw.end());
+    CL_DEBUG("processing an option: name=\""
+            << name << "\", value=\""
+            << value << "\"");
+
+    // call the appropriate handler
+    const THandler hdl = hit->second;
+    hdl(name, value);
+}
+
+void loadConfigString(const string &cnf)
+{
     if (cnf.empty())
         return;
 
-    if (string("no_error_recovery") == cnf) {
-        CL_DEBUG("parseConfigString: \"no_error_recovery\" mode requested");
-        data.errorRecoveryMode = /* no_error_recovery */ 0;
-        return;
-    }
+    // split the given config string into comma-separated options
+    std::vector<string> opts;
+    boost::split(opts, cnf, boost::algorithm::is_any_of(","));
 
-    if (string("oom") == cnf) {
-        CL_DEBUG("parseConfigString: \"OOM simulation\" mode requested");
-        data.oomSimulation = true;
-        return;
-    }
-
-    if (string("track_uninit") == cnf) {
-        CL_DEBUG("parseConfigString: \"track_uninit\" mode requested");
-        data.trackUninit = true;
-        return;
-    }
-
-    // TODO: make it possible to handle both parameters together (comma
-    // separated list or whatever)
-    // TODO: document all the parameters somewhere
-    if (string("noplot") == cnf) {
-        CL_DEBUG("parseConfigString: \"noplot\" mode requested");
-        data.skipUserPlots = true;
-        return;
-    }
-
-    const char *cstr = cnf.c_str();
-    const char *elPrefix = "error_label:";
-    const size_t elPrefixLen = strlen(elPrefix);
-    if (!strncmp(cstr, elPrefix, elPrefixLen)) {
-        cstr += elPrefixLen;
-        CL_DEBUG("parseConfigString: error label is \"" << cstr << "\"");
-        data.errLabel = cstr;
-        return;
-    }
-
-    CL_WARN("unhandled config string: \"" << cnf << "\"");
+    // go through all options and parse them one by one
+    ConfigStringParser parser;
+    BOOST_FOREACH(const string &str, opts)
+        parser.handleRawOption(str);
 }
 
 } // namespace GlConf
