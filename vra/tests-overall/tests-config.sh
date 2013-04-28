fa6664314a9ed3d1c91033201408fe339900700e
Kamil Dudka <kdudka@redhat.com>
2013-04-28 16:27:47 +0200

    vra/tests-overall: make it possible to use system GCC

--- vra/tests-overall/tests-config.sh
+++ vra/tests-overall/tests-config.sh
@@ -9,9 +9,12 @@
 # Path to the tests (without ending '/').
 TESTS_PATH="./"
 
+# Use $GCC_HOST from environment if set, local GCC build otherwise.
+test -n "$GCC_HOST" || GCC_HOST="../../gcc-install/bin/gcc"
+
 # Command that runs the analysis. The tests are run by using
 # `$ANALYSIS_CMD $TEST_FILE`.
-ANALYSIS_CMD="../../gcc-install/bin/gcc -fplugin=../../vra_build/libvra.so"
+ANALYSIS_CMD="$GCC_HOST -fplugin=../../vra_build/libvra.so"
 
 # Regular expression describing test files (format is that of `find`).
 TEST_REGEXP="test-[0-9]*.c"
