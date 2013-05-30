dcac64a7bc97f6cf480eb7bc4b3d3e5bf04878c3
Kamil Dudka <kdudka@redhat.com>
2013-05-30 17:13:32 +0200

    sl/adt_op_meta: new API for ADT meta-operations [not tested]

---
+++ sl/adt_op_meta.cc
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2013 Kamil Dudka <kdudka@redhat.com>
+ *
+ * This file is part of predator.
+ *
+ * predator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * predator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with predator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+#include "adt_op_meta.hh"
+
+using FixedPoint::TObjectMapper;
+
+namespace AdtOp {
+
+template <EDirection dir>
+bool projectMetaOperation(MetaOperation &mo, FixedPoint::TObjectMapper idMap)
+{
+    // TODO
+    return false;
+}
+
+} // namespace AdtOp
