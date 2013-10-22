581910f473bac31a23202f36f48d5094ff38504b
Kamil Dudka <kdudka@redhat.com>
2013-10-22 15:44:00 +0200

    Revert "sl/config.h: decrease SE_MAX_CALL_DEPTH to 0x10"
    This reverts commit 83ef608faefd39e7e9620ddb18b6e758b96f4a30.

--- sl/config.h
+++ sl/config.h
@@ -231,7 +231,7 @@
 /**
  * maximal call depth
  */
-#define SE_MAX_CALL_DEPTH                   0x10
+#define SE_MAX_CALL_DEPTH                   0x40
 
 /**
  * if non-zero, plot each state that caused an error to be reported
