0235624966bc70fc0087fa57c531636a09eaf3af
Kamil Dudka <kdudka@redhat.com>
2014-04-15 10:43:29 +0200

    tests/containers: exercise push_back() by ref/val

---
+++ tests/containers/push_back_val.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+struct Node {
+    int d;
+    struct Node* n;
+    struct Node* b;
+};
+
+#define NEW(type) (type *) malloc(sizeof(type))
+
+int main()
+{
+    struct Node* h = NULL;
+    struct Node* t = NULL;
+
+    for (;;) {
+        struct Node *p = NEW(struct Node);
+        if (NULL == h)
+            h = p;
+        else
+            t->n = p;
+
+        p->n = NULL;
+        p->d = 0;
+        p->b = t;
+
+        t = p;
+    }
+
+    return 0;
+}
