d4be3255c16459cac0f59c7d7e99b08c073396c2
Dee'Kej <deekej@linuxmail.org>
2014-07-03 18:03:10 +0200

    cl/gcc-adapt-ext: Test-case for ISO C11 introduced.
    This is only a dummy test-case for sanity reasons. The implementation
    of the test-case itself should be done for gcc 4.9.0 and higher.
    The previous versions doesn't support the C11 standard completely.

---
+++ tests/gcc-adapter/C/45-Standard-header-files-C11.c
@@ -0,0 +1,13 @@
+/* Test case: #45 */
+
+/**
+ * NOTE: This is only a dummy test-case for now. Many C11 features were not
+ *       available for gcc 4.5.x, which is currently used for initial support
+ *       development.
+ * TODO: Implement this test-case when testing gcc >= 4.8.x.
+ */
+
+int main(void)
+{
+  return 0;
+}
