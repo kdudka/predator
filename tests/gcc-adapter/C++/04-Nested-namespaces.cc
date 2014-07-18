4a802eb8be1b7cf8c6c133f702af7a9065a7c80c
Dee'Kej <deekej@linuxmail.org>
2014-07-18 17:37:22 +0200

    cl/gcc-adapt-ext: legal notices added to C++ tests in the test suite

--- tests/gcc-adapter/C++/04-Nested-namespaces.cc
+++ tests/gcc-adapter/C++/04-Nested-namespaces.cc
@@ -1,4 +1,20 @@
-/* Test-case: #04 */
+/* Test case: #04
+ *
+ * Copyright NOTE: This file is part of predator's test suite.
+ *
+ * predator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * predator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with predator. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 namespace nsX {
   char c = 42;
--- tests/gcc-adapter/C++/13-Using-declaration-03.cc
+++ tests/gcc-adapter/C++/13-Using-declaration-03.cc
@@ -1,12 +1,22 @@
-/* Test-case: #XX */
+/* Test case: #13
+ *
+ * Copyright NOTE: This file is part of predator's test suite.
+ *
+ * However, the example(s) below were taken from (and modified):
+ * <http://en.cppreference.com/>
+ *
+ * Because of it, this file is licensed under GFDL v1.3 and CC BY-SA 3.0
+ * licenses as requested at: <http://en.cppreference.com/w/Cppreference:FAQ>
+ *
+ * You should have receive copies of these licenses in the ~/predator/tests/
+ * folder with the predator itself. If not, please, see their online versions:
+ * <http://www.gnu.org/copyleft/fdl.html>
+ * <https://creativecommons.org/licenses/by-sa/3.0/legalcode>
+ */
 
 char c = '&';
 int i = 42;
 
-/*
- * Example taken from (with some modifications):
- * http://en.cppreference.com/w/cpp/language/namespace#Using-declarations
- */
 namespace A {
     int x;
 }
