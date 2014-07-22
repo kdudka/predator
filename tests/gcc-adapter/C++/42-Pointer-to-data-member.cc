808a67adce1895165df351550ce6520ba081d1e3
Dee'Kej <deekej@linuxmail.org>
2014-07-23 00:08:42 +0200

    cl/gcc-adapt-ext: OFFSET_TYPE testcase added
    From displaying the low-level GIMPLE it can be seen, that C++ parser
    currently internally transforms the address of pointer-to-data-member
    to an offset represented as an unsigned number instead of address.
    This results the accessing the pointee doesn't result in syntax error
    of adding two pointers together.

---
+++ tests/gcc-adapter/C++/42-Pointer-to-data-member.cc
@@ -0,0 +1,54 @@
+/* Test case: #42
+ *
+ * Copyright NOTE: This file is part of predator's test suite.
+ *
+ * predator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * predator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with predator. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <new>
+
+struct S {
+  int S::* i_ptr;
+  int i;
+
+  S() : i_ptr(0), i('#')
+  {
+    return;
+  }
+
+  S(const int i) : i_ptr(0), i(i)
+  {
+    return;
+  }
+};
+
+// // // // // // // // // // // // // // // // // // // // // // // // // // //
+
+int main()
+{
+  S s = S('$');               // C++98 initialization.
+
+  s.i_ptr = &S::i;            // Set the pointer to have offset of 'i'.
+
+  int &i_ref = s.*s.i_ptr;    // Try to get address of 'i' of object 's'.
+
+  // Pointer arithmetics should be now used to obtain the address of 'c' via
+  // 'x', so it can be changed. We're making sure 2 pointers are not added
+  // together, which is syntax error. If so, predator/forester should be able to
+  // catch this problem and OFFSET_TYPE handling would have to be fixed.
+  // FIXME: Find a way to test this possible issue right in the Code Listener.
+  i_ref = '@';
+
+  return i_ref;
+}
