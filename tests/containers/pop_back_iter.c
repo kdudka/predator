780e97051fa363cd8ddde6069ebc37ca7c3a0fc7
Kamil Dudka <kdudka@redhat.com>
2014-08-20 11:33:21 +0200

    tests/containers/pop_back_iter.c: test focused on iteration

---
+++ tests/containers/pop_back_iter.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int d;
+    struct Node* n;
+    struct Node* b;
+};
+
+#define NEW(type) (type *) malloc(sizeof(type))
+
+int __VERIFIER_nondet_int(void);
+
+int main()
+{
+    struct Node* h = NULL;
+    struct Node* t = NULL;
+
+    while (__VERIFIER_nondet_int()) {
+        struct Node *p = NEW(struct Node);
+        p->d = __VERIFIER_nondet_int();
+        if (NULL == h)
+            h = p;
+        else
+            t->n = p;
+        p->n = NULL;
+        p->b = t;
+        t = p;
+    }
+
+    struct Node* it = h;
+    while (it) {
+        printf("%d\n", it->d);
+        it = it->n;
+    }
+
+    it = t;
+    while (it) {
+        printf("%d\n", it->d);
+        it = it->b;
+    }
+
+    while (t) {
+        struct Node *p = t->b;
+        if (p)
+            p->n = NULL;
+        else
+            h = NULL;
+        free(t);
+        t = p;
+    }
+
+    return 0;
+}
