4f692f32aa8f7e8a946fb707109c069b1ead71ee
Kamil Dudka <kdudka@redhat.com>
2014-11-11 13:39:40 +0100

    switch-host-gcc.sh: exit unsuccessfully on test-suite failure

--- switch-host-gcc.sh
+++ switch-host-gcc.sh
@@ -80,6 +80,6 @@ build_analyzer() {
     $MAKE -C $1 check CTEST='ctest -j9'
 }
 
-build_analyzer fwnull fwnull
-build_analyzer sl Predator
-build_analyzer fa Forester
+build_analyzer fwnull fwnull                        || exit $?
+build_analyzer sl Predator                          || exit $?
+build_analyzer fa Forester                          || exit $?
