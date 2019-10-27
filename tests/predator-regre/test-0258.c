852b871356e8720d069b49ca8bd6ca662d033940
versokova <ver.sokova@gmail.com>
2019-10-27 18:07:38 +0100

    tests/predator-regre: cover resize realloc by tests
    Closes #22

---
+++ tests/predator-regre/test-0258.c
@@ -0,0 +1,36 @@
+#include <verifier-builtins.h>
+#include <stdlib.h>
+
+int main() {
+    int num;
+    if (__VERIFIER_nondet_int())
+        num = 2;
+    else
+        num = 5;
+
+    int *a = (int *) malloc(sizeof(int) * num);
+    if (a == NULL) 
+        return 0;
+    int *b = &(a[1]);
+    int *tmp = (int *) realloc(a, sizeof(int) * 3);
+    if (tmp == NULL) {
+        free(a);
+        return 0;
+    }
+    if (a != tmp)
+        tmp[1] = 2;
+    else 
+        *b = 3; // valid
+    free(tmp);
+    return 0;
+}
+
+/**
+ * @file test-0258.c
+ *
+ * @brief a regression test for reused memory by realloc()
+ *
+ * @attention
+ * This description is automatically imported from tests/predator-regre/README.
+ * Any changes made to this comment will be thrown away on the next import.
+ */
