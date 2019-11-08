cfc7c96cc60d76a3d8bb691fa79567eb0c90d760
Kamil Dudka <kdudka@redhat.com>
2019-11-08 16:13:55 +0100

    fixup! sl/symproc: make realloc(malloc(n), n) work in debug build
    cosmetic change

--- sl/symproc.cc
+++ sl/symproc.cc
@@ -1549,12 +1549,12 @@ bool SymExecCore::resizeObject(const TValId valAddr, const TSizeRange size)
     }
 
     TSizeRange oldsize = valSizeOfTarget(sh_, valAddr);
-    int diff = oldsize.lo - size.lo;
+    const int diff = oldsize.lo - size.lo;
     if (!diff)
         // resize to the same size is a no-op
         return true;
 
-    if (diff >= 0) { // reduce block, check for memory leaks in invalid area
+    if (diff > 0) { // reduce block, check for memory leaks in invalid area
         // <-------oldsize------>
         // <--size---><-invalid->
         LeakMonitor lm(sh_);
