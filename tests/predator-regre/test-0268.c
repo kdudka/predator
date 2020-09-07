a6743c3361f896c5e69728cbacfaa470b8f5159f
versokova <ver.sokova@gmail.com>
2020-09-07 12:52:58 +0200

    tests/predator-regre: added test for empty block befor aborted block
    Closes: https://github.com/kdudka/predator/pull/45

---
+++ tests/predator-regre/test-0268.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int *p = malloc(sizeof(int));
+
+    if (__VERIFIER_nondet_int()) {
+label:
+        abort();
+    }
+
+    free(p);
+    return 0;
+}
+
+/**
+ * @file test-0268.c
+ *
+ * @brief regression test for emty block before aborted block
+ *
+ * - contributed by Tomas Jasek
+ *
+ * @attention
+ * This description is automatically imported from tests/predator-regre/README.
+ * Any changes made to this comment will be thrown away on the next import.
+ */
