fc7056e42061e1340f761bd25ba626fa975cba39
Lukáš Zaoral <lzaoral@redhat.com>
2025-02-06 13:23:06 +0100

    cmake: use upstream BoostConfig.cmake unconditionally
    Fixes the following deprecation warning:
    ```
    CMake Warning (dev) at ./build-aux/common.cmake:33 (find_package):
      Policy CMP0167 is not set: The FindBoost module is removed.  Run "cmake
      --help-policy CMP0167" for policy details.  Use the cmake_policy command to
      set the policy and suppress this warning.
    ```

--- build-aux/common.cmake
+++ build-aux/common.cmake
@@ -28,9 +28,10 @@ if(NOT ENABLE_LLVM)
 endif()
 
 # Check Boost availability
+set(Boost_VERBOSE ON)
 set(Boost_USE_STATIC_LIBS ON)
 set(Boost_ADDITIONAL_VERSIONS "1.46" "1.47" "1.48" "1.49")
-find_package(Boost 1.37)
+find_package(Boost 1.37 CONFIG REQUIRED)
 if(Boost_FOUND)
     link_directories(${Boost_LIBRARY_DIRS})
     include_directories(SYSTEM ${Boost_INCLUDE_DIRS})
